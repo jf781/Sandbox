0cfb8655f4d2ffd2357fafc85b86b8cb744d8e73
Joe Fecht <joe.fecht@ahead.com>
2022-02-22 16:14:47 -0600

    Adding test app

---
+++ test-app/app.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {
+    organization = "CourageAI"
+    
+    workspaces {
+      name = "CourageAI-Infrastructure"
+    }
+  }
+}
+provider "aws" {
+  region = "us-west-2"
+}
+resource "aws_instance" "server-1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name   = "server"
+  }
+}
