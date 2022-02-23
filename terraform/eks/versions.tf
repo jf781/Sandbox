db974e5faea677cc6f75a23bbc6da57c9fba7648
Joe Fecht <joe.fecht@ahead.com>
2022-02-23 16:16:18 -0600

    Updated variables

--- terraform/eks/versions.tf
+++ terraform/eks/versions.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = "3.74.3"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "3.1.0"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = "2.0.0"
+      version = "2.1.0"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = "3.0.0"
+      version = "3.1.0"
     }
 
     template = {
