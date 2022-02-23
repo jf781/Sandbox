431b03a808884289d0cbb6d234cde1d3d6231178
Joe Fecht <joe.fecht@ahead.com>
2022-02-23 14:41:58 -0600

    Added vars file

---
+++ test-app/vars.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "Region"
+  type = string
+}
+
+variable "access_key_id" {
+  description = "AWS Access ID"
+  type = string
+}
+
+variable "access_key_secret" {
+  description = "AWS Access Secret"
+  type = string
+}
